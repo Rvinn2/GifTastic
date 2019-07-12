e3769eafa87db6be5af36b641bf05e9d89cddf16
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-07-11 23:59:13 -0400

    incomp.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,98 +1,36 @@
-$("button").on("click", function() {
-    
-    var animal = $(this).attr("data-animal");
+var animals = ["duck", "dog", "cow", "cat", 
+];
+// console.log(animals); WORKS
+function renderButtons() {
 
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=26hg8EUlhIJWYocCSV8vD86UmSPK8Uui" +
-      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-      
-    })
-      
-      .then(function(response) {
-        console.log(queryURL);
-
-        console.log(response);
-        var results = response.data;
-
-       
-        for (var i = 0; i < results.length; i++) {
-
-          
-          var animalDiv = $("<div>");
-
-          
-          var p = $("<p>").text("Rating: " + results[i].rating);
-
-         
-          var animalImage = $("<img>");
-          
-          animalImage.attr("src", results[i].images.fixed_height.url);
-
-          
-          animalDiv.append(p);
-          animalDiv.append(animalImage);
-
-          
-          $("#gifs-appear-here").prepend(animalDiv);
-        }
-      });
-  });
+  $("#buttons-view").empty();
 
 
-
-
-
-
-// $("#dogBtn").on("click", Function(){
   
 
-// }
+for (var i = 0; i < topics.length; i++) {
+    var zooAnimal = $("<button class = 'button'>");
 
 
+    zooAnimal.addClass("aniGif");
 
+    zooAnimal.attr("data-name", topics[i]);
 
+    zooAnimal.text(topics[i]);
+    $("#buttons-view").append(zooAnimal);
+    }
 
+        displayGiphy();
+}
 
-
-
-// $(document).ready(function(){
-    
-//     var animals = ["Lion", "Tiger", "Bear", "Dog", "Cat", "Moose"];
-
-
-
-//     function displayAnimals(arrayToUse, classToAdd, areaToAdd){
-//         $(areaToAdd).empty();
-
-//         for (var i =0; i < arrayToUse.length; i++){
-
-//             var a = $("<button>");
-//             a.addClass(classToAdd);
-//             a.attr("data-type", arrayToUse[i]);
-//             a.text(arrayToUse[i]);
-
-//             $(areaToAddTo).append(a);
-//         }
-
-//     }
-
-//     // giphy images from API
-
-//     $(document.onabort("click", ".animal-button", function(){
+function displayGiphy() {
+    $("button").on("click", function () {
+  
         
-//         $("#images").empty();
-
-//         $(".animal-button").removeclass("active");
-//         $(this).addClass("active");
-
-//         var type = $(this).attr("data-type");
-//         var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
-//         var queryURL = "api.giphy.com/v1/gifs/search=api_key:	"
-
-//     } ))
-// })
+        var aniGif = $(this).attr("data-name");
+     
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + aniGif + "&api_key=dc6zaTOxFJmzC&limit=10";
+         
+         
+          console.log("aniGif" + aniGif);
+          
